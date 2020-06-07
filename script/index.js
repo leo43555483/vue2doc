ebd6ba23a6403b4e6bffcc7bbe5c7f46eca55c87
zhlmac <664995185@qq.com>
2020-06-07 19:43:00 +0800

    output event and props

--- script/index.js
+++ script/index.js
@@ -3,13 +3,17 @@
 const program = require('commander');
 const semver = require('semver');
 const chalk = require('chalk');
+const execa = require('execa');
+const path = require('path');
 
 const checkNodeVersion = (expect, target) => {
   if (!semver.satisfies(process.version, expect)) {
-    console.log(chalk.red(`
+    console.log(
+      chalk.red(`
       You are using Node ${process.version}, ${target} requires Node ${expect}\n
       please upgrade your Node version.
-    `));
+    `)
+    );
     process.exit(1);
   }
 };
@@ -19,15 +23,25 @@ program
   .version(`${require('../package.json').version}`, '-v, --version')
   .usage('<command> [options]');
 
-
 program
   .command('create [entryPpath]')
   .description('build vue documents')
   .option('--config <configPath>', 'Build the project through config')
   .allowUnknownOption()
-  .action((entryPpath, cmd) => {
-    console.log(chalk.green('building documents...'));
-    require('../src/create.js')(entryPpath, cmd.config);
+  .action((entryPpath = '', cmd) => {
+    console.log(chalk.green('initializing...'));
+    const buildFilePath = path.join(__dirname, './create.js');
+    const args = [buildFilePath, entryPpath, cmd.config || ''];
+    execa('node', args, { stdio: 'inherit', cwd: process.cwd() })
+      .then(result => {
+        if (result.failed) {
+          process.exit(1);
+        }
+        console.log(chalk.green('success!'));
+      })
+      .catch(e => {
+        console.log(chalk.red('build failed'));
+      });
   });
 
 program.parse(process.argv);
--- src/parsejs.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = function (resource) {
-  // console.log(chalk.green('jsparse,'), resource);
-  return resource;
-};
