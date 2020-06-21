64a4927d380415434b4e4af694173422949cac06
zhlmac <664995185@qq.com>
2020-06-21 18:38:05 +0800

    fixed methods render

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ['plugin:vue/essential', 'airbnb-base'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
+    'no-plusplus': 'off',
+    'comma-dangle': 'off',
     'no-underscore-dangle': 'off',
     'no-param-reassign': ['error', { props: false }],
     'no-restricted-syntax': 0,
     'import/export': 0,
     'global-require': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+  // plugins: ['prettier'],
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
